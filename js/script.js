40c46d2a59a96f7c0c47c2345beed43fe3e892aa
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-08 22:26:56 +0200

    fix: css e js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
 // alert ("calc")
 
 const numbers = document.querySelectorAll (".tasto");
-console.log (numbers);
+// console.log (numbers);
 const command = document.querySelectorAll (".command");
-console.log (command);
+// console.log (command);
 const display = document.querySelector (".schermo");
-console.log (display);
+// console.log (display);
 
-let num1 = "";
 
 for (let i = 0; i < numbers.length; i++){
     let numbersBnt = numbers [i];
     console.log (numbersBnt);
     numbersBnt.addEventListener ("click", function (){  
-        num1 = numbersBnt.innerHTML;
-        console.log (this.innerHTML);
+        numbersBnt.innerHTML;
         display.innerHTML += numbersBnt.innerHTML;
-
     })
 }
